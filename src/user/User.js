40b40731b2331718f0b4987178df44a09d715c69
aolowude <andrew@3wp.io>
2020-02-07 06:14:07 +0100

    Update User.js

--- src/user/User.js
+++ src/user/User.js
@@ -1,5 +1,5 @@
 //import AsyncStorage from '@react-native-community/async-storage';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, View } from 'react-native';
 
 import Data from '../Data';
 import { hashPassword } from '../../lib/utils';
